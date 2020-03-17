063751423a0b03e4130566e52ce39bec4a7d25ec
Kyle Davis <kpd328@users.noreply.github.com>
2020-03-17 11:46:35 -0600

    swapped characterinfo from a flexlayout to a stacklayout, may end up just using a grid later

--- FifthCharacter/FifthCharacter/App.xaml.cs
+++ FifthCharacter/FifthCharacter/App.xaml.cs
@@ -10,7 +10,7 @@ namespace FifthCharacter {
         public App() {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new MainPage());
         }
 
         protected override void OnStart() {
