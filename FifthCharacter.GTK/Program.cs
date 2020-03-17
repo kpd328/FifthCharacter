a22f3c5a66497f0e429407023d6292e7ab663bb1
Kyle Davis <kpd328@users.noreply.github.com>
2020-03-16 20:15:25 -0600

    Updated nugets and added gtk# project

---
+++ FifthCharacter.GTK/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.GTK;
+
+namespace FifthCharacter.GTK {
+    class MainClass {
+        [STAThread]
+        public static void Main(string[] args) {
+            Gtk.Application.Init();
+            Forms.Init();
+
+            var app = new App();
+            var window = new FormsWindow();
+            window.LoadApplication(app);
+            window.SetApplicationTitle("Fifth Character");
+            window.Show();
+
+            Gtk.Application.Run();
+        }
+    }
+}
