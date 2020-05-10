7aac0880700d11815c8c4a8c3a6c4a79898758b5
Kyle Davis <kpd328@users.noreply.github.com>
2020-05-10 15:39:46 -0600

    Added a "no background" background for whatever reason.

---
+++ WotC.FifthEd.SRD/Backgrounds/NoBackground.cs
@@ -0,0 +1,10 @@
+﻿using FifthCharacter.Plugin.Interface;
+
+namespace WotC.FifthEd.SRD.Backgrounds {
+    public class NoBackground : IBackground {
+        public string Name => "No Background";
+        public string ID => "SRD.Background.None";
+
+        public IBackground GetInstance() => new NoBackground();
+    }
+}
