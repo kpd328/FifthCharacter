4d1f8279bdc544e1f7d5abe4d1d1344d33889b63
Kyle Davis <kpd328@users.noreply.github.com>
2020-08-20 11:04:42 -0700

    Fixes #30 using the solution described by #41, but that didn't fix it, turns out HighElf.GetInstance() was calling new HighElf() instead of new HighElf(true) [HighElf.cs:34]... oops...

--- WotC.FifthEd.SRD/Race/HighElf.cs
+++ WotC.FifthEd.SRD/Race/HighElf.cs
@@ -2,14 +2,14 @@
 using FifthCharacter.Plugin.Interface;
 using FifthCharacter.Plugin.Popup;
 using FifthCharacter.Plugin.StatsManager;
+using Syncfusion.XForms.Buttons;
 using System.Collections.Generic;
+using System.Linq;
 using WotC.FifthEd.SRD.Features.Race.Elf.High;
 using WotC.FifthEd.SRD.Proficiencies.Attacks.MartialMeleeWeapon;
 using WotC.FifthEd.SRD.Proficiencies.Attacks.MartialRangedWeapon;
 using WotC.FifthEd.SRD.Proficiencies.Attacks.SimpleRangedWeapon;
-using WotC.FifthEd.SRD.Proficiencies.Languages;
 using Xamarin.Forms;
-using Xamarin.Forms.Markup;
 
 namespace WotC.FifthEd.SRD.Race {
     public class HighElf : AElf {
@@ -29,47 +29,89 @@ namespace WotC.FifthEd.SRD.Race {
             ProficiencyManager.Proficiencies.Add(new ProfSRWShortbow(SOURCE_TEXT));
             ProficiencyManager.Proficiencies.Add(new ProfMRWLongbow(SOURCE_TEXT));
             PluginLoader = PluginLoader.GetLoader();
-            LanguageChoices = new List<IProficiency>(PluginLoader.Proficiencies.GetAllForType(ProficiencyType.LANGUAGE));
-            LanguageChoices.RemoveAll(l => l.GetType() == typeof(ProfLangCommon) || l.GetType() == typeof(ProfLangElvish));
         }
 
-        public override IRace GetInstance() => new HighElf();
+        public override IRace GetInstance() => new HighElf(true);
 
+
+        private IList<IProficiency> ChosenLanguages { get; set; } = new List<IProficiency>();
+        private int SelectedCount = 0;
+        private const int TOTAL_LANG = 1;
+        private SfChipGroup LanguageChoices;
         public override void BuildPopup(PopupNCRaceOptions raceOptions) {
-            Picker lang = new Picker() {
-                Title = "Extra Language",
-                ItemsSource = LanguageChoices,
-                BindingContext = this,
-                ItemDisplayBinding = new Binding("Name")
+            List<IProficiency> choices = PluginLoader.Proficiencies.GetAllForType(ProficiencyType.LANGUAGE)
+                .Where(p => p.ID != "SRD.Proficiency.Language.Common"
+                && p.ID != "SRD.Proficiency.Language.Elvish").ToList();
+            for (int i = 0; i < choices.Count; i++) {
+                if (ProficiencyManager.CheckByName(choices[i].Name)) {
+                    choices.RemoveAt(i);
+                    i--;
+                }
+            }
+
+            var langTitle = new Label() {
+                Text = "Choose an extra Language",
+                HorizontalOptions = LayoutOptions.StartAndExpand,
+                VerticalOptions = LayoutOptions.Start
+                //TODO: format
             };
-            lang.SetBinding(Picker.SelectedItemProperty, "SelectedLanguage");
-            lang.Row(0);
-            lang.Column(0);
-            lang.ColumnSpan(2);
-            raceOptions.Body.Children.Add(lang);
+
+            LanguageChoices = new SfChipGroup() {
+                Type = SfChipsType.Filter,
+                ItemsSource = choices,
+                DisplayMemberPath = "Name",
+                ChipLayout = new FlexLayout() {
+                    Direction = FlexDirection.Row,
+                    Wrap = FlexWrap.Wrap,
+                    HorizontalOptions = LayoutOptions.FillAndExpand,
+                    VerticalOptions = LayoutOptions.FillAndExpand,
+                    AlignContent = FlexAlignContent.SpaceEvenly,
+                    JustifyContent = FlexJustify.Start,
+                    AlignItems = FlexAlignItems.Start
+                }
+            };
+            LanguageChoices.SelectionChanging += LanguageChoices_SelectionChanging;
+
+            StackLayout stackLayout = new StackLayout() {
+                Orientation = StackOrientation.Vertical
+            };
+            stackLayout.Children.Add(langTitle);
+            stackLayout.Children.Add(LanguageChoices);
+            raceOptions.Body.Children.Add(stackLayout);
         }
 
-        public override void BuildPopup(PopupNCRaceOptions_GTK raceOptions) {
-            Picker lang = new Picker() {
-                Title = "Extra Language",
-                ItemsSource = LanguageChoices,
-                BindingContext = this,
-                ItemDisplayBinding = new Binding("Name")
-            };
-            lang.SetBinding(Picker.SelectedItemProperty, "SelectedLanguage");
-            lang.Row(0);
-            lang.Column(0);
-            lang.ColumnSpan(2);
-            raceOptions.Body.Children.Add(lang);
+        private void LanguageChoices_SelectionChanging(object sender, Syncfusion.Buttons.XForms.SfChip.SelectionChangingEventArgs e) {
+            if (SelectedCount >= TOTAL_LANG) {
+                e.Cancel = true;
+            }
+            if (e.AddedItem != null && !e.Cancel) {
+                ChosenLanguages.Add(e.AddedItem as IProficiency);
+                SelectedCount++;
+            }
+            if (e.RemovedItem != null) {
+                ChosenLanguages.Remove(e.RemovedItem as IProficiency);
+                SelectedCount--;
+
+            }
+            if (SelectedCount >= TOTAL_LANG) {
+                foreach (var c in LanguageChoices.GetChips()) {
+                    if (!c.IsChecked) {
+                        c.IsCheckable = false;
+                    }
+                }
+            }
+            if (SelectedCount < TOTAL_LANG) {
+                foreach (var c in LanguageChoices.GetChips()) {
+                    c.IsCheckable = true;
+                }
+            }
         }
 
         public override void ConfirmPopup() {
-            SelectedLanguage.Source = SOURCE_TEXT;
-            ProficiencyManager.Proficiencies.Add(SelectedLanguage);
+            foreach (var s in ChosenLanguages) {
+                s.Source = SOURCE_TEXT;
+                ProficiencyManager.Proficiencies.Add(s);
+            }
         }
-
-        //Popup stuff
-        public List<IProficiency> LanguageChoices;
-        public IProficiency SelectedLanguage { get; set; }
     }
 }
